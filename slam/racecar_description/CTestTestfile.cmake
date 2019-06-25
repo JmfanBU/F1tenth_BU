50f2ba6b42421bba2236826175acae1e15df9e33
ShidongS <43020747+ShidongS@users.noreply.github.com>
2019-06-25 11:26:15 -0400

    Add files via upload

---
+++ slam/racecar_description/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/f1/slam_ws/src/racecar_description
+# Build directory: /home/f1/slam_ws/build_isolated/racecar_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ slam/racecar_description/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/f1/slam_ws/devel_isolated/racecar_description:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/f1/slam_ws/devel_isolated/racecar_description/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/f1/slam_ws/src/racecar_description:$ROS_PACKAGE_PATH"
